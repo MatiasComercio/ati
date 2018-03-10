17dac2d9f9a234ac4fc2a4320df44d6696657a04
Juan <jpmrno@gmail.com>
2018-03-10 02:09:38 -0300

    remove unnecessary alert & ImageCropWithRubberband class

--- src/main/java/ar/edu/itba/ati/idp/ui/UnsavedChangesAlert.java
+++ src/main/java/ar/edu/itba/ati/idp/ui/UnsavedChangesAlert.java
@@ -3,13 +3,13 @@ package ar.edu.itba.ati.idp.ui;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
 
-public class UnsavedChangesCloseAlert extends Alert {
+public class UnsavedChangesAlert extends Alert {
 
-  public UnsavedChangesCloseAlert() {
+  public UnsavedChangesAlert(final String title, final String content) {
     super(AlertType.CONFIRMATION);
-    this.setTitle("You have unsaved changes");
+    this.setTitle(title);
     this.setHeaderText(null);
-    this.setContentText("Do you want to save the current image before closing it?");
+    this.setContentText(content);
     this.getButtonTypes().setAll(ButtonType.YES, ButtonType.NO, ButtonType.CANCEL);
   }
 }
